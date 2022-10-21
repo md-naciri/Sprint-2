791c469c5449b77c63c2163ac8860a194abbf6c7
md_naciri <med.ar.naciri@gmail.com>
2022-10-21 20:48:11 +0100

    reworking on the show and the add tasks

--- Brief1/assets/js/app.js
+++ Brief1/assets/js/app.js
@@ -1,234 +1,174 @@
-/**
- * In this file app.js you will find all CRUD functions name.
- * 
- */
 
-let title = document.getElementById("title");
-let radio1 = document.getElementById("radio1");
-let radio2 = document.getElementById("radio2");
-let selection1 = document.getElementById("selection1");
-let selection2 = document.getElementById("selection2");
-let date = document.getElementById("date");
-let description = document.getElementById("description");
-let toDo = document.getElementById("to-do");
-let InProgress = document.getElementById("in-progress");
-let done = document.getElementById("done");
-let save = document.getElementById("save");
-
-let carte = {};
-
-let stock = () => {
-    carte["id"] = 1;
-    carte["title"] = title.value;
-
-    if (radio1.checked == true) {
-        carte["type"] = "Feature";
-    }
-    else {
-        carte["type"] = "Bug";
-    }
-
-    if (selection1.value == "Low") {
-        carte["Priority"] = "Low";
-    }
-    else if (selection1.value == "Medium") {
-        carte["Priority"] = "Medium";
-    } else if (selection1.value == "High") {
-        carte["Priority"] = "High";
-    } else if (selection1.value == "Critical") {
-        carte["Priority"] = "Critical";
-    }
-
-    if (selection2.value =="To Do") {
-        carte["Status"] = "To Do";
-    }
-    else if (selection2.value == "In Progress") {
-        carte["Status"] = "In Progress";
-    } else if (selection2.value =="Done") {
-        carte["Status"] = "Done";
-    }
-
-    carte["date"] = date.value;
-
-    carte["description"] = description.value;
-}
-
-
-function affichage() {
+const title = document.querySelector("#title");
+const radio1 = document.querySelector("#radio1");
+const radio2 = document.querySelector("#radio2");
+const selection1 = document.querySelector("#selection1");
+const selection2 = document.querySelector("#selection2");
+const date = document.querySelector("#date");
+const description = document.querySelector("#description");
+const toDo = document.querySelector("#to-do");
+const inProgress = document.querySelector("#in-progress");
+const done = document.querySelector("#done");
+let toDoCount = document.querySelector("#toDoCount");
+let inProgressCount = document.querySelector("#inProgressCount");
+let doneCount = document.querySelector("#doneCount");
+const form = document.querySelector("#form");
+const save = document.querySelector("#save");
+function initTaskForm() {
     toDo.innerHTML = "";
-    InProgress.innerHTML = "";
+    inProgress.innerHTML = "";
     done.innerHTML = "";
-    
-    for (let task of tasks) {
-        let shortDesc = task.description;
+    let tod = 0;
+    let inp = 0;
+    let doe = 0;
+    for (let card of tasks) {
+        let shortDesc = card.description;
         if (shortDesc.length > 65) {
         shortDesc = `${shortDesc.substring(0, 65)}...`;
         }
-        if (task.status === "To Do") {
+        if (card.status === "To Do") {
+            tod++;
             toDo.innerHTML +=
        ` 
        <button class="w-100 d-flex bg-white py-2">
-        <div class="">
-            <i class="fa-regular fa-circle-question text-green"></i>
-        </div>
-        <div class="text-start ps-2">
-            <div class="fw-bolder">${task.title}</div>
             <div class="">
-                <div class="text-secondary">${task.id}Created in ${task.date}</div>
-                <div class="" title="${task.description}">${shortDesc}</div>
+                <i class="fa-regular fa-circle-question text-green"></i>
             </div>
-            <div class="">
-                <span class="btn btn-primary py-1 px-2">${task.priority}</span>
-                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${task.type}</span>
+            <div class="text-start ps-2">
+                <div class="fw-bolder">${card.title}</div>
+                <div class="">
+                    <div class="text-secondary">#${card.id+1} Created in ${card.date}</div>
+                    <div class="" title="${card.description}">${shortDesc}</div>
+                </div>
+                <div class="">
+                    <span class="btn btn-primary py-1 px-2">${card.priority}</span>
+                    <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${card.type}</span>
+                </div>
             </div>
-        </div>
-        <div>
-            <div class = "" style="flex-direction: column;display: flex;justify-content: flex-start;">
-                <i onclick="deleteTask(this);" class="fa-solid fa-trash fs-4"></i>
-                <i onclick="editTask(${task.id});" data-bs-target="#modal-task" data-bs-toggle="modal" class="fa-solid fa-pen-to-square fs-4"></i>
-            </div>
-        </div>
+            <i onclick="deleteTask(${card.id});" class="fa-regular fa-x text-red deleteX"></i>
+            <i onclick="editTask(${card.id});" class="fa-solid fa-pencil"></i>
         </button>`; 
-        } else if (task.status === "In Progress"){
-            InProgress.innerHTML +=
+        } else if (card.status === "In Progress"){
+            inp++;
+            inProgress.innerHTML +=
             `<button class="w-100 d-flex bg-white py-2">
         <div class="">
             <i class="fa-regular fa-circle-question text-green"></i>
         </div>
         <div class="text-start ps-2">
-            <div class="fw-bolder">${task.title}</div>
+            <div class="fw-bolder">${card.title}</div>
             <div class="">
-                <div class="text-secondary">${task.id}Created in ${task.date}</div>
-                <div class="" title="${task.description}">${shortDesc}</div>
+                <div class="text-secondary">#${card.id+1} Created in ${card.date}</div>
+                <div class="" title="${card.description}">${shortDesc}</div>
             </div>
             <div class="">
-                <span onclick="deleteTask(this);" class="btn btn-primary py-1 px-2">${task.priority}</span>
-                <span onclick="editTask(this);" class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${task.type}</span>
+                <span class="btn btn-primary py-1 px-2">${card.priority}</span>
+                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${card.type}</span>
             </div>
         </div>
+        <i onclick="deleteTask(${card.id});" class="fa-regular fa-x text-red deleteX"></i>
         </button>`
-        } else if (task.status === "Done"){
+        } else if (card.status === "Done"){
+            doe++;
             done.innerHTML +=
             `<button class="w-100 d-flex bg-white py-2">
         <div class="">
             <i class="fa-regular fa-circle-question text-green"></i>
         </div>
         <div class="text-start ps-2">
-            <div class="fw-bolder">${task.title}</div>
+            <div class="fw-bolder">${card.title}</div>
             <div class="">
-                <div class="text-secondary">${task.id}Created in ${task.date}</div>
-                <div class="" title="${task.description}">${shortDesc}</div>
+                <div class="text-secondary">#${card.id+1} Created in ${card.date}</div>
+                <div class="" title="${card.description}">${shortDesc}</div>
             </div>
             <div class="">
-                <span   onclick="deleteTask(this);" class="btn btn-primary py-1 px-2">${task.priority}</span>
-                <span   onclick="editTask(${task.id});"  class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${task.type}</span>
+                <span class="btn btn-primary py-1 px-2">${card.priority}</span>
+                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${card.type}</span>
             </div>
         </div>
+        <i onclick="deleteTask(${card.id});" class="fa-regular fa-x text-red deleteX"></i>
         </button>`
         }
-    } 
+    }
+    toDoCount.innerHTML = tod;
+    inProgressCount.innerHTML = inp;
+    doneCount.innerHTML = doe;
 }
-
-affichage();
+initTaskForm();
 
 function createTask() {
-    // initialiser task form
+    $("#modal-task").modal("show");
+}
 
-    // Afficher le boutton save
 
-    // Ouvrir modal form
+function saveTask() {
+ 
+    card = {
+        'id'            :   tasks.length,
+        'title'         :   title.value,
+        'type'          :   '',
+        'priority'      :   selection1.value,
+        'status'        :   selection2.value,
+        'date'          :   date.value,
+        'description'   :   description.value,
+    }
+    if (radio1.checked == true) {
+        card["type"] = "Feature";
+    }
+    else {
+        card["type"] = "Bug";
+    }
+    tasks.push(card);
+    initTaskForm();
+    reloadTasks();
 
 }
-function saveTask() {
-    stock();
-    tasks[tasks.length] = carte;
-    let shortDesc = carte["description"];
-    if (shortDesc.length > 65) {
-        shortDesc = `${shortDesc.substring(0, 65)}...`;
-    }
-    if (carte["Status"] == "To Do") {
-        toDo.innerHTML +=
-            `<button class="w-100 d-flex bg-white py-2">
-        <div class="">
-            <i class="fa-regular fa-circle-question text-green"></i>
-        </div>
-        <div class="text-start ps-2">
-            <div class="fw-bolder">${carte["title"]}</div>
-            <div class="">
-                <div class="text-secondary">Created in ${carte["date"]}</div>
-                <div class="" title="${carte["description"]}">${shortDesc}</div>
-            </div>
-            <div class="">
-                <span class="btn btn-primary py-1 px-2">${carte["Priority"]}</span>
-                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${carte["type"]}</span>
-            </div>
-        </div>
-        </button>`
-    }
-    else if (carte["Status"] == "In Progress") {
-        InProgress.innerHTML +=
-            `<button class="w-100 d-flex bg-white py-2">
-        <div class="">
-            <i class="fa-regular fa-circle-question text-green"></i>
-        </div>
-        <div class="text-start ps-2">
-            <div class="fw-bolder">${carte["title"]}</div>
-            <div class="">
-                <div class="text-secondary">Created in ${carte["date"]}</div>
-                <div class="" title="${carte["description"]}">${shortDesc}</div>
-            </div>
-            <div class="">
-                <span class="btn btn-primary py-1 px-2">${carte["Priority"]}</span>
-                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${carte["type"]}</span>
-            </div>
-        </div>
-        </button>`
-    }
-    else if (carte["Status"] == "Done") {
-        done.innerHTML +=
-            `<button class="w-100 d-flex bg-white py-2">
-        <div class="">
-            <i class="fa-regular fa-circle-question text-green"></i>
-        </div>
-        <div class="text-start ps-2">
-            <div class="fw-bolder">${carte["title"]}</div>
-            <div class="">
-                <div class="text-secondary">Created in ${carte["date"]}</div>
-                <div class="" title="${carte["description"]}">${shortDesc}</div>
-            </div>
-            <div class="">
-                <span class="btn btn-primary py-1 px-2">${carte["Priority"]}</span>
-                <span class="btnG btn py-1 px-2" style="background-color: var(--buttonGray)">${carte["type"]}</span>
-            </div>
-        </div>
-        </button>`
-    }
-    reloadTasks();
+
+
+function deleteTask(delCard) {
+    tasks = tasks.filter(function(card){
+        return card.id != delCard;
+    })
+    initTaskForm();
 }
 
 function editTask(index) {
-    // let selectedtask=index.parentElement.parentElement.parentElement;
     
-    if (tasks[index-1].type == "Feature") {
+    title.value = tasks[index].title;
+    description.value = tasks[index].description;
+    date.value = tasks[index].date;
+    if (tasks[index].type == "Feature") {
         radio1.checked = true;
     }
     else {
         radio2.checked = true;
     }
-    // title.value=selectedtask.children[1].children[0].innerHTML;
-    // description.value=tasks[index].description.innerHTML;
-    title.value=tasks[index-1].title;
-    description.value=tasks[index-1].description;
-    // console.log(index);
-    // console.log(tasks[index-1].title);
-    // console.log(tasks[index-1].description);
-    // selection1.value=tasks[index-1].selection1;
-    // selection2.value=tasks[index-1].selection2;
+    selection1.value = tasks[index].priority;
+    selection2.value = tasks[index].status;
+    createTask();
+    save.addEventListener("click", ()=> {
+       let card = {
+            'id'            :   index+1,
+            'title'         :   title.value,
+            'type'          :   '',
+            'priority'      :   selection1.value,
+            'status'        :   selection2.value,
+            'date'          :   date.value,
+            'description'   :   description.value,
+        }
+        if (radio1.checked == true) {
+            card["type"] = "Feature";
+        }
+        else {
+            card["type"] = "Bug";
+        }
 
+    tasks[index] = card;
+    initTaskForm();
+    });
 }
 
 function updateTask() {
-
     // GET TASK ATTRIBUTES FROM INPUTS
 
     // Créez task object
@@ -238,25 +178,11 @@ function updateTask() {
     // Fermer Modal form
 
     // Refresh tasks
-
-}
-
-function deleteTask(del) {
-    del.parentElement.parentElement.parentElement.remove();
     
 }
 
-function initTaskForm() {
-    // Clear task form from data
 
-    // Hide all action buttons
-}
 
 function reloadTasks() {
-    title.value = "";
-    radio1.checked;
-    selection1.value = 3;
-    selection2.value = 0;
-    date.value = "";
-    description.value = "";
+    form.reset();
 }
