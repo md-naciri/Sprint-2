a824a835b197328c1a0f23241e51179c5a831a90
md_naciri <med.ar.naciri@gmail.com>
2022-11-14 17:26:19 +0100

    Working on the maquette and the log in page

---
+++ Brief3/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+ include('head.php');
+?>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <div class="navbar-brand ms-3"><strong>Origin Game</strong></div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="me-3">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="font-weight-bold nav-link" style="color: #007074;" href="logIn.php"><strong>Log in</strong></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" style="color: #007074;" href="#"><strong>Create account</strong></a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </nav>
+      <div class="px-3 pb-4 text-center">
+        <img src="img/home.jpg" class="rounded img-fluid" alt="Home image">
+      </div>
+</body>
+</html>
---
+++ Brief3/logIn.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+ include('head.php');
+?>
+<body>
+    <div class="row">
+        <div class="col-4">
+            <img class="w-100" src="img/logIn.jpg" alt="">
+        </div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <h1 class="text-center">Welcome to Origin Gamer</h1>
+                    <label for="inputEmail4" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Enter your Email here">
+                    <label for="inputPassword4" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Enter your Password">
+                    <button class="login-button">Log in</button>
+                    <p>Or create account?<a href="">Sign up</a></p>
+                </div>
+                <div class="col-2"></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
