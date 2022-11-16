5597f5d146c14b06ea0456f9b3be5902d99a2241
md_naciri <med.ar.naciri@gmail.com>
2022-11-16 12:09:53 +0100

    Improving the architecture of the project

---
+++ Brief3/pages/dashboard.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <?php 
+    include('head.php');
+    ?>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <div class="navbar-brand ms-3"><strong>Origin Game</strong></div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="me-3">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" style="color: #007074;" href="#"><strong>Log out</strong></a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </nav>
+      <div class="px-3 pb-4 text-center">
+        
+      </div>
+</body>
+</html>
--- Brief3/pages/logIn.php
+++ Brief3/pages/logIn.php
@@ -2,36 +2,35 @@
 <html lang="en">
 <head>
     <?php 
-    include('pages/head.php');
+    include('head.php');
     ?>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
     <div class="row">
+        <div class="col-5 log-image">
+            <img class="w-100" src="../assets/img/logIn.jpg" alt="">
+        </div>
         <div class="col-7 d-flex justify-content-center align-items-center">
             <div class="row col-12">
                 <div class="col-2"></div>
                 <div class="col-8 ">
                     <h1 class="text-center mar">Welcome to Origin Gamer</h1>
                     <form action="scripts.php" method="POST">
-                        <label for="inputName" class="form-label">Full Name</label>
-                        <input name="name" type="text" class="form-control mar" id="inputName" placeholder="Enter your Full Name">
                         <label for="inputEmail4" class="form-label">Email</label>
                         <input name="email" type="email" class="form-control mar" id="inputEmail4" placeholder="Enter your Email here">
                         <label for="inputPassword4" class="form-label">Password</label>
                         <input name="password" type="password" class="form-control mar" id="inputPassword4" placeholder="Enter your Password">
                         <div class="text-center">
-                            <button type="submit" name="save" class="login-button">Create Account</button>
-                            <p>Already have an account?&nbsp;<a href="pages/logIn.php">Log in</a></p>
+                            <button type="submit" name="log-in" class="login-button">Log in</button>
+                            <p>Or create account?&nbsp;<a href="../index.php">Sign up</a></p>
                         </div>
                     </form>
 
                 </div>
                 <div class="col-2"></div>
             </div>
-        </div>
-        <div class="col-5 log-image">
-            <img class="w-100" src="assets/img/signUp.jpg" alt="">
+
         </div>
     </div>
 </body>
